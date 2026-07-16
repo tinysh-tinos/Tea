990907f6481df0bd692fa2834f38b53aa712406f
tinysh-tinos <maksimgordienko599@gmail.com>
2026-07-16 13:27:39 +0800

    Update main.cpp

--- chayok/src/main.cpp
+++ chayok/src/main.cpp
@@ -1,42 +1,26 @@
 #include "Arduino.h"
 #include "HX711.h"
-
-#define photo A0
-#define water 2
 #define DT  A0
 #define SCK A1
-
 HX711 scale;                                                  
-
 float calibration_factor = -14.15;                            
 float units;                                                  
 float ounces;                                                 
-
 void setup() {
-  pinMode(photo, INPUT);
-  pinMode(water, INPUT);
   scale.begin(DT, SCK);                                       
   scale.set_scale();                                          
   scale.tare();                                               
   scale.set_scale(calibration_factor);    
   Serial.begin(9600);
 }
-
-void serial_loop(){
-  int a = analogRead(photo);
-  int b = analogRead(water);                            
+void serial_loop(){                          
   for (int i = 0; i < 10; i ++) {                              
     units = + scale.get_units(), 10;                          
   }
   units = units / 10;                                          
   ounces = units * 0.035274;                                  
   Serial.print(ounces);                                       
-
-  Serial.print(a);
-  Serial.print(", ");
-  Serial.print(b);
 }
-
 void loop(){
   serial_loop();
   delay(100);
