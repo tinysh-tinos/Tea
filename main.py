abc70868bdb0104321fdb7e9efb9462f353cb520
tinysh-tinos <maksimgordienko599@gmail.com>
2026-07-16 13:23:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import serial
 import time
 import csv
-import os
-
-# Открываем Serial-порт (замените 'COM3' на ваш порт, например '/dev/ttyACM0' для Linux)
 arduino = serial.Serial('COM1', 9600, timeout=1)
-
-# Даем плате время на перезагрузку
 time.sleep(2)
-
 try:
     while True:
-        # Считываем строку из порта
-        data = [['water_level' , 'light_sensor'] 
-                [arduino.readline()]
-                ]
-        
-        # Если данные есть, декодируем их из байтов в строку и убираем лишние пробелы
-        if data:
-            decoded_data = data.decode('utf-8').strip()
+        line = arduino.readline()
+        if line:
+            decoded_data = line.decode('utf-8').strip()
             print(decoded_data)
+            with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow(['main'])
+                writer.writerow([decoded_data])
+            time.sleep(1)            
 except KeyboardInterrupt:
-    # Закрываем соединение при остановке программы
     arduino.close()
     print("Порт закрыт")
-
-# Открываем файл для записи. encoding='utf-8' важен для русских букв
-with open('public/data.csv', mode='w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    
-    # Записываем строки
-    writer.writerows(data)
